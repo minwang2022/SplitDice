78a4e5e39f2ea5d637d926d2e0f5db0d59df8e96
min <minwang.bus@gmail.com>
2021-05-10 15:32:48 -0400

    working on controller

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+class User < ApplicationRecord
+    validates :username, :session_token, :password_digest, :email, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    validates :username, uniqueness: true
+  
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+
+
+
+    #USER Auth#
+
+    
+    def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+    end
+
+    def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+    end
+
+    private
+
+    def new_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+    
+    def generate_unique_session_token
+        self.session_token = new_session_token
+        while User.find_by(session_token: self.session_token)
+            self.session_token = new_session_token
+        end
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_unique_session_token
+    end
+
+end
