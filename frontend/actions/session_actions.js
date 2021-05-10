f037b1a3ee42fe28e3cc20ca0f5bacfa54d1d2e0
min <minwang.bus@gmail.com>
2021-05-10 17:58:10 -0400

    debugged a huge bug related to import export, work on component part

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,46 @@
+import * as Util from '../Util/session_api_Util.js';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "CLEAR_ERRORS";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const receiveCurrentUser = currentUser => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER,
+});
+
+export const receiveErrors = errors => {
+  return {
+    type: RECEIVE_ERRORS,
+    errors
+  };
+};
+
+export const login = user => dispatch =>(
+    Util.login(user)
+    .then( user => (dispatch(receiveCurrentUser(user))),
+        err => (dispatch(receiveErrors(err.responseJSON)))
+    )
+);
+
+export const logout = () => dispatch =>(
+    Util.logout()
+    .then( () => (dispatch(logoutCurrentUser())),
+        err => (dispatch(receiveErrors(err.responseJSON)))
+    )
+);
+
+export const signUp = user => dispatch =>(
+    Util.signUp(user)
+    .then( user => (dispatch(receiveCurrentUser(user))),
+        err => (dispatch(receiveErrors(err.responseJSON)))
+    )
+);
+
+
---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>SplitWise</h1>
+  </div>
+);
+
+export default App;
