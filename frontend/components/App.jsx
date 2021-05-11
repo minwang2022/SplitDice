e6355ed2784266403d770682c151b785069e3598
min <minwang.bus@gmail.com>
2021-05-10 20:56:47 -0400

    working on Greeting page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const App = () => (
   <div>
-    <h1>SplitWise</h1>
+        <header>
+            <h1>SplitWise</h1>
+            <GreetingContainer/>
+        </header>
   </div>
 );
 
---
+++ frontend/components/session_form/login_form_container.jsx
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { signUp } from '../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mSTP = 
