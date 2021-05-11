24c03c8e0c2316a29bdafe2d7adc257cec7d3a2d
min <minwang.bus@gmail.com>
2021-05-11 14:32:03 -0400

    deleted .js in session action for util import

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,4 +1,4 @@
-import * as Util from '../Util/session_api_util.js';
+import * as Util from '../Util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "CLEAR_ERRORS";
