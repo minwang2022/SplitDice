5fb6c869963b76fcbe03801b5caa318b2652907a
min <minwang.bus@gmail.com>
2021-05-11 16:01:52 -0400

    renaming home to home2

--- frontend/components/app2.jsx
+++ frontend/components/app2.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Switch, Link} from 'react-router-dom';
-import Homepage from './home/home';
+import Homepage from './home/home2';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
 
